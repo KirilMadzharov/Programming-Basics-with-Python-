e089ac171828e861c1f21e76740a8dc580066ba4
Kiril Madzharov <133707345+KirilMadzharov@users.noreply.github.com>
2023-10-23 23:32:45 +0100

    Delete 1.  First Steps in Coding directory

--- 1.  First Steps in Coding/1. Lab/05. Greeting by Name.py
+++
@@ -1,8 +0,0 @@
-"""
-Write a program that reads from the console text (name of a person)
-and prints "Hello, <name>!", where <name> is the name entered from the console.
-"""
-
-name = input()
-
-print("Hello, " + name + "!")
