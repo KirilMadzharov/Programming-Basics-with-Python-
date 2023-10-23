8df4562e677453c3be9d890b39bb40d1f143e4e7
Kiril Madzharov <133707345+KirilMadzharov@users.noreply.github.com>
2023-10-23 02:00:27 +0100

    Add files via upload

---
+++ 3. Conditional Statements Advanced/Overview of Week 3.py
@@ -0,0 +1,41 @@
+"""
+Week 3 Learning Overview: Conditional Statements Advanced
+
+Duration: 6 hours of lectures
+
+Problems Solved: A total of 31 problems from Lab, Exercise, and More Exercise sections.
+
+1. Logical Operators:
+
+and: Returns True if both conditions evaluated are true.
+or: Returns True if at least one of the conditions evaluated is true.
+not: Negates the truth value of the condition it precedes.
+
+2. Boolean Values:
+
+True: Represents a positive or affirmative condition.
+False: Represents a negative or non-affirmative condition.
+
+3. Nested Conditional Statements:
+
+Understanding how to embed conditional checks within other conditional checks, allowing for more
+granular and complex decision-making in code.
+
+
+Week 3's journey into Python programming has been pivotal, further building upon the foundational
+concepts introduced in the earlier weeks. This week's emphasis on logical operators unlocked the
+ability to craft intricate conditions, granting the program the agility to navigate through diverse scenarios.
+The synergy between and, or, and not was especially illuminating, showcasing the layered logic potential within coding.
+
+The intrinsic nature of True and False was central to this week's learnings.
+Beyond their basic boolean interpretations, they set the trajectory of the program's path based on diverse evaluations.
+
+Delving into nested conditional statements deepened the understanding of decision-making mechanisms in code.
+These multi-tiered checks introduce an added layer of depth to conditional logic. The hands-on approach
+of tackling 31 problems across various sections underscored this understanding, highlighting the real-world
+applications of nested checks.
+
+In wrapping up, week 3 has been an enriching continuation in the Python learning journey, spotlighting logic,
+boolean evaluations, and multi-layered decision-making structures. Engaging with a myriad of problems reinforced
+the practical understanding of these topics, setting the stage for upcoming modules.
+"""
